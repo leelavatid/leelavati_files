b08521262ed81f9581e660e41acd68f23f564c78
manik <manik@LAPTOP-7V35NJR1>
2024-10-29 08:04:18 +0530

    2nd commit

---
+++ svkp_college/src/basicsofjava/ArrayDemo.java
@@ -0,0 +1,38 @@
+package basicsofjava;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int num[]= {12,34,56,98};
+		System.out.println(num[0]);
+		System.out.println(num[3]);
+		System.out.println(num[1]);
+		byte b[]= {1,2,3,4,5,6};
+		System.out.println(b[0]);
+		System.out.println(b[4]);
+		System.out.println(b[5]);
+		short s[]= {10,20,30};
+		System.out.println(s[0]);
+		System.out.println(s[2]);
+		System.out.println(s[1]);
+		float f1[]= {0.9f,0.3f,7.90f};
+		System.out.println(f1[0]);
+		System.out.println(f1[1]);
+		double d1[]= {12.34d,56.78d,90.45d,89.72d};
+		System.out.println(d1[0]);
+		System.out.println(d1[3]);
+		System.out.println(d1[1]);
+		char c[]= {'m','f','l','h'};
+		System.out.println(c[0]);
+		System.out.println(c[3]);
+		System.out.println(c[1]);
+		long l1[]= {5678987789765l,7876567849849838l,84434886663748l};
+		System.out.println(l1[0]);
+		System.out.println(l1[2]);
+		System.out.println(l1[1]);
+		
+			
+		}
+	}
+
+
---
+++ svkp_college/src/basicsofjava/StringDemo.java
@@ -0,0 +1,20 @@
+package basicsofjava;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String str1="Java Programming Language";
+		String str3="HTML&CSS&JS";
+		String str4="SQL";
+		String str5="JSP.NET";
+		String str2="Communication Skills";
+		System.out.println(str1);
+		System.out.println(str3);
+		System.out.println(str4);
+		System.out.println(str5);
+		System.out.println(str2);
+		
+
+	}
+
+}
